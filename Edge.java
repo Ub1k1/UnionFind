87e775e125be69971aafdc575863ebbad545f5da
Ub1k1 <63819671+Ub1k1@users.noreply.github.com>
2020-09-22 15:41:18 -0400

    Create Edge.java

---
+++ Edge.java
@@ -0,0 +1,11 @@
+public class Edge {
+    public int to;
+    public int cost;
+    public int from;
+
+    public Edge(int i, int t, int c){
+        to = t;
+        cost = c;
+        from = i;
+    }
+}
